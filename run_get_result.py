7f3d66454b69b0e4ec9591f801fbc113d5a36bf5
tang <zb19851222@qq.com>
2019-05-06 10:38:39 +0800

    i modified

--- run_get_result.py
+++ run_get_result.py
@@ -3,7 +3,7 @@ from utility.data_tool.data_post_process import PostProcessTool
 PostProcessTool.from_bert_key_work_extract_get_res_to_txt_sord_and_clearn_them("data/res/extract_key_word_output","data/res/res_extract_keyword.txt","data/raw_data/test.txt",False)
 
 #submission的raw data 到 标准结果。
-PostProcessTool.get_core_res("data/res/res_extract_keyword.txt","data/res/submission_output","data/res/all_core.txt")
+PostProcessTool.get_core_res("data/res/res_extract_keyword2test.txt","data/res/submission_output","data/res/all_core.txt")
 
 #将emotion和core 结果合并。
-PostProcessTool.merge_core_emotion_finally("data/res/all_core.txt","data/res/extract_key_word_output","data/res/emotion_output","data/res/result.txt")
+PostProcessTool.merge_core_emotion_finally("data/res/all_core.txt","data/res/extract_key_word_output/from_test_data","data/res/emotion_output","data/res/result.txt")
